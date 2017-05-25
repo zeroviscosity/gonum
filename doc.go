f34e20dd4ac044c6f166d657bc1c46641e9e0e6a
Brendan Tracey <tracey.brendan@gmail.com>
2017-05-25 09:55:17 -0600

    Add remaining canonical import paths

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Copyright ©2017 The gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// gonum is a set of packages implementing numeric and scientific algorithms
+// for the Go programming language.
+package gonum // import gonum.org/v1/gonum
